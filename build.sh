6d70f379afd9553732b64065fa2c4ff1c9559d60
KarinaMaharani <maharani.k4rina21@gmail.com>
2026-05-31 09:14:38 +0700

    Fix Vercel Python build configuration

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-pip install -r requirements.txt
+python -m pip install --break-system-packages -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
-
-# Run migrations
-python manage.py migrate --noinput
